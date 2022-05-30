e8220d96dfb6c8ee2ee120d6495ddd64dc3a8cd8
e11824496 <e11824496@student.tuwien.ac.at>
2022-05-30 21:44:55 +0200

    minor fixes and cleaned up readme

--- container/README.txt
+++ container/README.txt
@@ -3,4 +3,8 @@ docker build -t dic-assignment .
 
 winpty docker run --rm -it -p 5000:5000 dic-assignment
 
-curl http://localhost:5000/api/detect -F images=@./container/images/dogos.jpg > asdf.jpg
+curl http://localhost:5000/api/detect -F images=@./images/dogos.jpg > asdf.jpg
+
+
+# Detect all images in folder (run within container)
+source run_inference.sh images output
--- container/app.py
+++ container/app.py
@@ -89,27 +89,26 @@ def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
 
     for i in range(min(boxes.shape[0], max_boxes)):
         if scores[i] >= min_score:
-        ymin, xmin, ymax, xmax = tuple(boxes[i])
-        display_str = "{}: {}%".format(class_names[i].decode("ascii"),
-                                        int(100 * scores[i]))
-        color = colors[hash(class_names[i]) % len(colors)]
-        image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
-        draw_bounding_box_on_image(
-            image_pil,
-            ymin,
-            xmin,
-            ymax,
-            xmax,
-            color,
-            font,
-            display_str_list=[display_str])
-        np.copyto(image, np.array(image_pil))
+            ymin, xmin, ymax, xmax = tuple(boxes[i])
+            display_str = "{}: {}%".format(class_names[i].decode("ascii"),
+                                            int(100 * scores[i]))
+            color = colors[hash(class_names[i]) % len(colors)]
+            image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+            draw_bounding_box_on_image(
+                image_pil,
+                ymin,
+                xmin,
+                ymax,
+                xmax,
+                color,
+                font,
+                display_str_list=[display_str])
+            np.copyto(image, np.array(image_pil))
     return image
 
 
 def detection_loop(filename_image):
     result_images = {}
-    print('asdf')
     for filename, image in filename_image.items(): 
         converted_img  = tf.image.convert_image_dtype(image, tf.float32)[tf.newaxis, ...]
         result = detector(converted_img)
@@ -122,10 +121,8 @@ def detection_loop(filename_image):
             np.array(image), result["detection_boxes"],
             result["detection_class_entities"], result["detection_scores"])
 
-
         result_images[filename] = image_with_boxes
         
-
     return result_images
 
 
