642e5b20c393cb63c8bc47e5b05a7aef9658ccd8
e11824496 <e11824496@student.tuwien.ac.at>
2022-05-30 21:29:13 +0200

    add script to detect all files in folder

---
+++ container/run_inference.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+InputFolder=$1
+OutputFolder=$2
+
+mkdir -p $OutputFolder
+
+for FILE in "$InputFolder"/*; do 
+    filename=$(basename -- "$FILE")
+    filename="${filename%.*}"
+    echo "$OutputFolder/$filename.jpg"
+    curl http://localhost:5000/api/detect -F images=@"$FILE" > "$OutputFolder/$filename.jpg"
+done
